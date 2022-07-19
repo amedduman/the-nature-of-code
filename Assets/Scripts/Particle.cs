f5f5a26b274f15223ae1bdb4a0103d4356d854e2
amed <amedduman@protonmail.com>
2022-07-19 18:20:31 +0300

    End of 2.5

--- Assets/Scripts/Particle.cs
+++ Assets/Scripts/Particle.cs
@@ -4,123 +4,45 @@ using UnityEngine;
 
 public class Particle : MonoBehaviour
 {
-    [SerializeField] Vector3 _gravity = new Vector3(0, -9.8f, 0);
-    [SerializeField] Vector3 _wind = new Vector3(1, 0, 0);
-    [SerializeField] [Min(.1f)] float _mass = .1f;
-    [SerializeField] float _mu = .1f; // friction factor of surfaces
-    [SerializeField] float _c = .1f; // drag factor
-    float _bottomEdgeHeight;
-    Vector3 _vel = Vector3.zero;
-    Vector3 _acc = Vector3.zero;
+    public float _mass = 1;
 
-    private void Start()
+    [SerializeField] Vector3 _vel = new Vector2(1,1);
+    [SerializeField] float _velLimit = 1;
+    [SerializeField] Attractor _attractor;
+
+    Vector3 _acc = Vector3.zero; 
+
+    void Start()
     {
-        _bottomEdgeHeight = -Camera.main.orthographicSize;
-        SetRadius(Mathf.Sqrt(_mass));
+        _velLimit = Random.Range(5, 15);
+        Time.timeScale = 10;
+        _vel = Random.insideUnitCircle * _velLimit;
+        //GetComponentInChildren<TrailRenderer>().material.color = Random.ColorHSV();
     }
 
-    void SetRadius(float r)
-    {
-        transform.localScale = new Vector3(r, r, 1);
-    }
 
     void Update()
     {
         _acc = Vector3.zero;
 
-        ApplyForce(_gravity * _mass);
+        float attraction = _attractor.Attract(this);
 
-        if(Input.GetKey(KeyCode.Mouse0))
-        {
-            ApplyForce(_wind);
-        }
+        Vector3 dir = (_attractor.transform.position - transform.position).normalized;
 
-        if(Input.GetKey(KeyCode.Mouse1))
-        {
-            ApplyForce(_wind * -1);
-        }
+        ApplyForce(dir * attraction);
         
-        Edges();
-
-        Friction();
-
-        if(transform.position.y < 0)
-        {
-            Drag();
-        }
-
         _vel += _acc;
 
-        transform.position += _vel * Time.deltaTime;;
+        _vel = Vector3.ClampMagnitude(_vel, _velLimit);
+
+        //Debug.DrawRay(transform.position, _vel);
+
+        transform.position += _vel * Time.deltaTime; 
     }
 
     void ApplyForce(Vector3 force)
     {
         Vector3 acc = force / _mass;
         _acc += acc *Time.deltaTime;
-    }
-
-    void Edges()
-    {
-        Vector3 pos = transform.position;
-
-        float up = Camera.main.orthographicSize;
-        float bottom = -Camera.main.orthographicSize;
-        float right = Camera.main.orthographicSize;
-        float left = -Camera.main.orthographicSize;
-
-        
-
-        if(pos.y  <= bottom)
-        {
-            pos.y = bottom;
-            transform.position = pos;
-            _vel.y *= -1;
-        }
-        else if(pos.y  >= up)
-        {
-            pos.y = up;
-            transform.position = pos;
-            _vel.y *= -1;
-        }
-        if(pos.x  <= left)
-        {
-            pos.x = left;
-            transform.position = pos;
-            _vel.x *= -1;
-        }
-        else if(pos.x  >= right) 
-        {
-            pos.x = right;
-            transform.position = pos;
-            _vel.x *= -1;
-        }
-    }
-
-    void Friction()
-    {
-        float height = transform.position.y;
-        float bottom = -Camera.main.orthographicSize;
-
-        Vector3 friction = _vel;
-        friction = friction.normalized;
-        friction *= -1;
-        friction *= _mu;
-        friction *= _mass;
-
-        ApplyForce(friction);
-    }
-
-    void Drag()
-    {
-        Vector3 drag = _vel;
-        drag.Normalize();
-        drag *= -1;
-
-        float speed = _vel.magnitude;
-
-        drag *= speed * speed * _c;
-
-        ApplyForce(drag);
     }
 }
