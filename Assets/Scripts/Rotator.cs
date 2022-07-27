85c208d821de9414f9b290fd7edd2ed28ba44300
amed <amedduman@protonmail.com>
2022-07-27 15:18:00 +0300

    End of 3.1

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float _degree = 0;
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _degree += Time.deltaTime;
+        transform.Rotate(new Vector3(0, 0, _degree));
     }
 }
