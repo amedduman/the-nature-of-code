8df27427334087a37f37c89ade1e8d9071995363
amed <amedduman@protonmail.com>
2022-07-30 19:57:38 +0300

    End of 3.3

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    [ReadOnly] [SerializeField] float _angle;
-    [SerializeField] float _angleVel;
-    [ReadOnly] [SerializeField] float _angleAcc;
+    [SerializeField] float _speed = 10;
 
     void Update()
     {
-        _angleAcc = 0;
-        float mouseX = Input.GetAxis("Mouse X");
-        mouseX = Mathf.Clamp(mouseX, -0.1f, 0.1f);
-        _angleAcc = mouseX;
-        _angleVel += _angleAcc;
-        _angle += _angleVel * Time.deltaTime;
-        transform.Rotate(new Vector3(0, 0, _angle));
+        Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 vel = target - transform.position;
+
+        vel = vel.normalized;
+        vel.z = 0;
+
+        float angle = Mathf.Atan2(vel.y, vel.x) * (180 / Mathf.PI) - 90; 
+
+        transform.position += vel * Time.deltaTime * _speed;  
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
